b6805717fcd567f3e084b1d66019fce8acb6865b
NewAryabro <138558338+NewAryabro@users.noreply.github.com>
2025-01-30 01:31:12 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -115,7 +115,7 @@ User - {}"""
 ⭐ Rating: {rating} / 10</b>
 """
 
-    FILE_CAPTION = """<b>{file_name}\n\n𝖩𝗈𝗂𝗇➥ 「<a href="https://t.me/Telugu_Movies_999">Telugu Movies 999</a>」</b>"""
+    FILE_CAPTION = """<b>{file_name}\n\n𝖩𝗈𝗂𝗇➥ 「<a href="https://t.me/Telugu_Movies_999">𝙏𝙀𝙇𝙐𝙂𝙐 𝙈𝙊𝙑𝙄𝙀𝙎 999</a>」</b>"""
     
 
     ALRT_TXT = """ᴊᴀʟᴅɪ ʏᴇʜᴀ sᴇ ʜᴀᴛᴏ !"""
